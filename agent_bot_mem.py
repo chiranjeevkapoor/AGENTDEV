e52279dfe7d7591daf97e1714a85a898604686eb
Chiranjeev Kapoor <kapoorchiranjeev@gmail.com>
2026-06-07 15:49:41 +0530

    loggs_stored

--- agent_bot_mem.py
+++ agent_bot_mem.py
@@ -40,4 +40,15 @@ while user_input != "exit":
     user_input = input("Enter :")
     
 
+with open("logging.txt", "w") as file:
+    file.write("Your conversation log:\n")
+    for message in conversation_history:
+        if isinstance(message, HumanMessage):
+            file.write(f"You : {message.content}\n")
+        elif isinstance(message, AIMessage):
+            file.write(f"AI : {message.content}\n\n")
+    file.write("Conversation is over.")
+
+print("Conversation saved to logging.txt")
+
     
